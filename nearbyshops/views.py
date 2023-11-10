a7ca1107343aa3279c9243d0dee042228710b84e
dee-do <dhanashree.desai@gmail.com>
2023-11-10 10:35:02 -0800

    completed nearbyCafes code

---
+++ nearbyshops/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+
+latitude = 47.667096
+longitude = -122.349554
+user_location = Point(longitude, latitude, srid=4326)
+
+# Create your views here.
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = "shops"
+    queryset = Shop.objects.annotate(distance = Distance("location", user_location)).order_by("distance")[0:6]
+    template_name = "shops/index.html"
+
---
+++ readme.txt
@@ -0,0 +1,9 @@
+This is a simple location based web app. 
+It lists cafes closest to a specific location, and displays them on a map. 
+
+
+Technologies:
+-Django for the web app.
+-GeoDjango sub-framework to implement geolocation features.
+-A spatial database (PostgreSQL and PostGIS).
+-Leaflet JS to display locations on a map.
